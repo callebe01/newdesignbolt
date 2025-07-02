428da31aed7174864f59f6d4afc52ac61dee03f1
callebe01 <161732013+callebe01@users.noreply.github.com>
2025-07-02 14:29:26 -0700

    Fix Vite build error for embed-widget.js

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['/embed-widget.js'],
       input: {
         main: path.resolve(__dirname, 'index.html'),
         'embed-widget': path.resolve(__dirname, 'src/embed/index.tsx'),
